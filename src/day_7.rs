9cf4710d0ee3e8a880c9a074cec34e26fd3570c9
Oliver With <oliverwith@bbv.ch>
2023-01-02 12:36:49 +0100

    parsing files

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, hash::Hash};
+use std::collections::HashMap;
 
 pub fn day_7_greeter() {
     println! {"Day 7"};
@@ -11,6 +11,59 @@ fn looper(file_name: &str) {
         for line in lines {
             if let Ok(items) = line {
                 println!("line {:?}", items);
+            }
+        }
+    }
+}
+
+use regex::Regex;
+fn split_file_listing(line: &str) -> (usize, String) {
+    let re = Regex::new(r"(^[0-9]*)\s+([^']+)").unwrap();
+    let caps = re.captures(line).unwrap();
+
+    (caps[1].parse::<usize>().unwrap(), String::from(&caps[2]))
+}
+
+#[derive(Debug, PartialEq)]
+enum ParserStates {
+    Cd,
+    Ls,
+    Limbo,
+}
+struct Parser<'p> {
+    state: ParserStates,
+    dir: &'p mut Dir,
+    current_dir: String,
+}
+
+impl<'p> Parser<'p> {
+    fn new(dir: &'p mut Dir) -> Parser<'p> {
+        Parser {
+            state: ParserStates::Limbo,
+            dir,
+            current_dir: String::from(""),
+        }
+    }
+
+    fn parse_line(&mut self, line: &str) {
+        if self.state == ParserStates::Limbo && line.contains("$ cd ") {
+            self.state = ParserStates::Cd;
+            self.current_dir = crate::utils::string_from_n_to_end(line, 5);
+            self.dir.name = crate::utils::string_from_n_to_end(line, 5);
+        } else if self.state != ParserStates::Cd && line.contains("$ cd ") {
+            self.state = ParserStates::Cd;
+            self.current_dir = crate::utils::string_from_n_to_end(line, 5);
+        } else if line.contains("ls") {
+            self.state = ParserStates::Ls;
+        } else if self.state == ParserStates::Ls {
+            let current_dir: &mut Dir = self.dir.cd(&self.current_dir).unwrap();
+
+            if line.contains("dir") {
+                current_dir.add_dir(&crate::utils::string_from_n_to_end(line, 4));
+            } else {
+                let (file_size, file_name) = split_file_listing(line);
+
+                current_dir.add_file(&file_name, file_size as u32);
             }
         }
     }
@@ -49,18 +102,9 @@ impl Dir {
         self.files.iter().map(|x| x.size).sum()
     }
 
-    // fn size_of_dir_including_subdirs(&self) -> u32 {
-    //     let mut size_of_subdirs: u32 = 0;
-    //     for (key, subdir) in &self.sub_dirs {
-    //         size_of_subdirs += subdir.size_of_dir_including_subdirs();
-    //     }
-
-    //     return self.size_of_files_in_dir() + size_of_subdirs;
-    // }
-
     fn record_size_of_dir_including_subdirs(&self, size_count: &mut HashMap<String, u32>) -> u32 {
         let mut size_of_subdirs: u32 = 0;
-        for (key, subdir) in &self.sub_dirs {
+        for (_, subdir) in &self.sub_dirs {
             size_of_subdirs += subdir.record_size_of_dir_including_subdirs(size_count);
         }
 
@@ -68,11 +112,73 @@ impl Dir {
         size_count.insert(String::from(&self.name), size_of_dir);
         return size_of_dir;
     }
+
+    fn cd(&mut self, i_name: &str) -> Option<&mut Dir> {
+        if self.name == i_name {
+            return Some(self);
+        } else {
+            for (_, subdir) in &mut self.sub_dirs {
+                match subdir.cd(i_name) {
+                    Some(x) => return Some(x),
+                    None => todo!(),
+                }
+            }
+            None
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn navigator_cd() {
+        let mut dir = Dir::make_empty_dir("");
+        let mut prs = Parser::new(&mut dir);
+        let mut accountant: HashMap<String, u32> = HashMap::new();
+
+        prs.parse_line("$ cd /");
+        assert_eq!(ParserStates::Cd, prs.state);
+
+        prs.parse_line("$ ls");
+        assert_eq!(ParserStates::Ls, prs.state);
+        prs.parse_line("dir a");
+        prs.parse_line("99 b.txt");
+
+        assert_eq!(
+            99,
+            prs.dir
+                .record_size_of_dir_including_subdirs(&mut accountant)
+        );
+        assert_eq!(99, *accountant.get("/").unwrap());
+
+        prs.parse_line("$ cd a");
+        assert_eq!(ParserStates::Cd, prs.state);
+        prs.parse_line("$ ls");
+        assert_eq!(ParserStates::Ls, prs.state);
+
+        prs.parse_line("dir e");
+        prs.parse_line("23 f");
+        prs.parse_line("33 g");
+        let current_dir = prs.dir.cd("a").unwrap();
+        assert_eq!(23 + 33, current_dir.size_of_files_in_dir());
+
+        prs.parse_line("$ cd e");
+        prs.parse_line("$ ls");
+        prs.parse_line("584 i");
+        let current_dir = prs.dir.cd("e").unwrap();
+        assert_eq!(584, current_dir.size_of_files_in_dir());
+
+    }
+
+    #[test]
+    fn split_file_listings() {
+        assert_eq!(
+            (8345, String::from("file.ending")),
+            split_file_listing("8345 file.ending")
+        );
+    }
 
     #[test]
     fn adding_root_dir() {
@@ -98,10 +204,7 @@ mod tests {
         let mut dir = Dir::make_empty_dir("root");
         dir.add_file("eins", 44);
         dir.add_dir("new_dir");
-        let current_dir = match dir.sub_dirs.get_mut("new_dir") {
-            Some(x) => x,
-            None => todo!(),
-        };
+        let current_dir = dir.cd("new_dir").unwrap();
         current_dir.add_file("sub_eins", 34);
 
         assert_eq!(1, current_dir.files.len());
@@ -116,24 +219,6 @@ mod tests {
 
         assert_eq!(34 + 44, dir.size_of_files_in_dir());
     }
-
-    // #[test]
-    // fn calc_size_of_subdirs() {
-    //     let mut dir = Dir::make_empty_dir("root");
-
-    //     dir.add_file("eins", 44);
-    //     dir.add_file("zwei", 34);
-    //     dir.add_dir("sub");
-    //     let subdir = dir.sub_dirs.get_mut("sub").unwrap();
-    //     subdir.add_file("sub_eins", 5);
-    //     subdir.add_file("sub_zwei", 7);
-    //     subdir.add_dir("subsub");
-    //     let subsubdir = subdir.sub_dirs.get_mut("subsub").unwrap();
-    //     subsubdir.add_file("subsub_eins", 3);
-    //     subsubdir.add_file("subsub_zwei", 8);
-
-    //     assert_eq!(44 + 34 + 5 + 7 + 3 + 8, dir.size_of_dir_including_subdirs());
-    // }
 
     #[test]
     fn pass_on_size_of_subdirs() {
--- src/utils.rs
+++ src/utils.rs
@@ -11,3 +11,25 @@ where
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
+
+pub fn string_from_n_to_m(i_string: &str, i_from: usize, i_to: usize) -> String {
+    String::from(&i_string[i_from..=i_to])
+}
+
+pub fn string_from_n_to_end(i_string: &str, i_from: usize) -> String {
+    String::from(&i_string[i_from..])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn string_parsing() {
+        assert_eq!(
+            "eins",
+            string_from_n_to_m("hahaeinszizi", 4, 7)
+        );
+
+        assert_eq!("meier", string_from_n_to_end("bliblameier", 6));
+    }
+}
