1daee82eeaab5a9ab805da645648ff7fc17cf92f
Oliver With <oliverwith@bbv.ch>
2023-01-02 09:32:08 +0100

    day 7 with help from  NickyMeuleman / scrapyard

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,62 +1,53 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, path::PathBuf};
 
 pub fn day_7_greeter() {
     println! {"Day 7"};
 
-    looper("./inputs/7_example.txt");
+    looper("./inputs/7_hot.txt");
 }
 
 fn looper(file_name: &str) {
+    let mut sizes: HashMap<PathBuf, usize> = HashMap::new();
+
+    let mut stich: Vec<String> = Vec::new();
+
     if let Ok(lines) = crate::utils::read_lines(file_name) {
         for line in lines {
             if let Ok(items) = line {
-                println!("line {:?}", items);
+                println!("{}", items);
+
+                if items.starts_with("$ ls") || items.starts_with("dir") {
+                    continue;
+                }
+                let splits: Vec<_> = items.split_whitespace().collect();
+
+                match splits.as_slice() {
+                    ["$", "cd", ".."] => {
+                        stich.pop();
+                    }
+
+                    ["$", "cd", name] => {
+                        stich.push(name.to_string());
+                    }
+                    [size, _name] => {
+                        let size: usize = size.parse().unwrap();
+                        for idx in 0..stich.len() {
+                            let current_path = PathBuf::from_iter(&stich[..=idx]);
+                            *sizes.entry(current_path).or_insert(0) += size;
+                        }
+                    }
+                    _ => {}
+                };
             }
         }
     }
-}
 
-struct File {
-    name: String,
-    size: u32,
-}
+    let sum_0: usize = sizes
+        .iter()
+        .map(|i| if *i.1 <= 100_000 { *i.1 } else { 0 })
+        .sum();
 
-struct Dir {
-    name: String,
-    files: Vec<File>,
-    sub_dirs: HashMap<String, Dir>,
-}
-
-impl Dir {
-    fn add_file(&mut self, name: &str, size: u32) {
-        self.files.push(File {
-            name: String::from(name),
-            size,
-        });
-    }
-    fn make_empty_dir(i_name: &str) -> Dir {
-        Dir {
-            name: String::from(i_name),
-            files: Vec::new(),
-            sub_dirs: HashMap::new(),
-        }
-    }
-    fn add_dir(&mut self, i_name: &str) {
-        self.sub_dirs
-            .insert(String::from(i_name), Dir::make_empty_dir(i_name));
-    }
-    fn size_of_files_in_dir(&self) -> u32 {
-        self.files.iter().map(|x| x.size).sum()
-    }
-
-    fn size_of_subdirs(&self) -> u32 {
-        let mut size_of_subdirs: u32 = 0;
-        for (key, subdir) in &self.sub_dirs {
-            size_of_subdirs += subdir.size_of_subdirs();
-        }
-
-        return self.size_of_files_in_dir() + size_of_subdirs;
-    }
+    println!("sum {}", sum_0);
 }
 
 #[cfg(test)]
@@ -64,63 +55,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn adding_root_dir() {
-        let dir = Dir::make_empty_dir("root");
-        assert_eq!("root", dir.name);
-        assert_eq!(0, dir.files.len());
-        assert_eq!(0, dir.sub_dirs.len());
-    }
-
-    #[test]
-    fn adding_files_and_dirs() {
-        let mut dir = Dir::make_empty_dir("root");
-
-        dir.add_file("eins", 44);
-        dir.add_file("zwei", 34);
-        dir.add_dir("new_dir");
-        assert_eq!(2, dir.files.len());
-        assert_eq!(1, dir.sub_dirs.len());
-    }
-
-    #[test]
-    fn cd_to_a_dir_add_files_and_dirs() {
-        let mut dir = Dir::make_empty_dir("root");
-        dir.add_file("eins", 44);
-        dir.add_dir("new_dir");
-        let current_dir = match dir.sub_dirs.get_mut("new_dir") {
-            Some(x) => x,
-            None => todo!(),
-        };
-        current_dir.add_file("sub_eins", 34);
-
-        assert_eq!(1, current_dir.files.len());
-    }
-    #[test]
-    fn calc_size_of_files() {
-        let mut dir = Dir::make_empty_dir("root");
-
-        dir.add_file("eins", 44);
-        dir.add_file("zwei", 34);
-        dir.add_dir("new_dir");
-
-        assert_eq!(34 + 44, dir.size_of_files_in_dir());
-    }
-
-    #[test]
-    fn calc_size_of_subdirs() {
-        let mut dir = Dir::make_empty_dir("root");
-
-        dir.add_file("eins", 44);
-        dir.add_file("zwei", 34);
-        dir.add_dir("sub");
-        let subdir = dir.sub_dirs.get_mut("sub").unwrap();
-        subdir.add_file("sub_eins", 5);
-        subdir.add_file("sub_zwei", 7);
-        subdir.add_dir("subsub");
-        let subsubdir = subdir.sub_dirs.get_mut("subsub").unwrap();
-        subsubdir.add_file("subsub_eins", 3);
-        subsubdir.add_file("subsub_zwei", 8);
-
-        assert_eq!(44 + 34 + 5 + 7 + 3 + 8, dir.size_of_subdirs());
+    fn sample() {
+        looper("./inputs/7_example.txt");
     }
 }
